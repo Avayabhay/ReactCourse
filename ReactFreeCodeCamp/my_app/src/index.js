539de704fab4b4a5efc094182a02ec840a46409d
Abhay Sharma <avay.abhay@gmail.com>
2021-07-20 17:06:44 +0530

    Making Book With 3 other Components

--- ReactFreeCodeCamp/my_app/src/index.js
+++ ReactFreeCodeCamp/my_app/src/index.js
@@ -1,20 +1,49 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-function Greeting() {
+function BookList() {
   return (
-    <>
-      <div className="row">
-        <First />
-      </div>
-      <Second />
-    </>
+    <section>
+      <h2>This is a BookList</h2>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 }
 
-const First = () => <h4>THis is my First Component</h4>;
-const Second = () => {
-  return <div>This is a Second Div</div>;
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
 };
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/9167i2ioFaS._AC_UY327_FMwebp_QL65_.jpg"
+    alt=""
+  />
+);
+const Title = () => (
+  <h4>
+    Karma: A Yogi's Guide to Crafting Your Destiny NEW YORK TIMES, USA TODAY
+  </h4>
+);
+const Author = () => {
+  return <h5>Sadhguru </h5>;
+};
+
+// const First = () => <h4>THis is my First Component</h4>;
+// const Second = () => {
+//   return <div>This is a Second Div</div>;
+// };
+
+ReactDom.render(<BookList />, document.getElementById("root"));
