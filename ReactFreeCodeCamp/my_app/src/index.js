c3e71d9b7bec61b84facdaa7ad8509f9d62466bd
Abhay Sharma <avay.abhay@gmail.com>
2021-07-20 16:34:29 +0530

    React.Fragment

--- ReactFreeCodeCamp/my_app/src/index.js
+++ ReactFreeCodeCamp/my_app/src/index.js
@@ -2,7 +2,14 @@ import React from "react";
 import ReactDom from "react-dom";
 
 function Greeting() {
-  return <h4>THis is my First Component</h4>;
+  return (
+    <React.Fragment>
+      <div className="row">
+        <h4>THis is my First Component</h4>;
+      </div>
+      <div>This is a Second Div</div>
+    </React.Fragment>
+  );
 }
 
 ReactDom.render(<Greeting />, document.getElementById("root"));
