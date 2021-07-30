427c622540a7ed04fa12b1b54599ccf2eeb7adde
Abhay Sharma <avay.abhay@gmail.com>
2021-07-30 09:47:54 +0530

    JSX JavaScript

--- ReactFreeCodeCamp/my_app/src/index.js
+++ ReactFreeCodeCamp/my_app/src/index.js
@@ -2,18 +2,16 @@ import React from "react";
 import ReactDom from "react-dom";
 //css
 import "./index.css";
+//Declearing vars
+const title =
+  "Karma: A Yogi's Guide to Crafting Your Destiny NEW YORK TIMES, USA TODAY";
+const author = "Sadhguru";
 
 function BookList() {
   return (
     <>
       <h2>This is a BookList</h2>
       <section className="booklist">
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
         <Book />
       </section>
     </>
@@ -23,31 +21,14 @@ function BookList() {
 const Book = () => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://m.media-amazon.com/images/I/9167i2ioFaS._AC_UY327_FMwebp_QL65_.jpg"
+        alt=""
+      />
+      <h4>{title}</h4>
+      <h5> {author.toUpperCase()} </h5>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src="https://m.media-amazon.com/images/I/9167i2ioFaS._AC_UY327_FMwebp_QL65_.jpg"
-    alt=""
-  />
-);
-const Title = () => (
-  <h4>
-    Karma: A Yogi's Guide to Crafting Your Destiny NEW YORK TIMES, USA TODAY
-  </h4>
-);
-const Author = () => {
-  return <h5 style={{ color: "#543" }}>Sadhguru </h5>;
-};
-
-// const First = () => <h4>THis is my First Component</h4>;
-// const Second = () => {
-//   return <div>This is a Second Div</div>;
-// };
 
 ReactDom.render(<BookList />, document.getElementById("root"));
