2bdd24aa30a4a629256b73f8418a0d2cb99d94ec
Abhay Sharma <avay.abhay@gmail.com>
2021-08-04 13:27:14 +0530

    useState Array Example

--- ReactFreeCodeCamp/Advance React/src/tutorial/1-useState/setup/3-useState-array.js
+++ ReactFreeCodeCamp/Advance React/src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,35 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+  const removeName = (id) => {
+    let newArray = people.filter((person) => person.id !== id);
+    setPeople(newArray);
+  };
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+
+        return (
+          <div className="item" key={id}>
+            <h4>{name}</h4>
+            <button
+              type="button"
+              className="btn"
+              onClick={() => removeName(id)}
+            >
+              Remove this Person
+            </button>
+          </div>
+        );
+      })}
+      <button type="button" className="btn" onClick={() => setPeople([])}>
+        Clear EveryThing
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
