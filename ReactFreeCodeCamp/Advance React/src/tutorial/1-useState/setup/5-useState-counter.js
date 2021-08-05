ce08c6fd6b2b9052471c51c30b46a477c1bdfcb9
Abhay Sharma <avay.abhay@gmail.com>
2021-08-05 22:11:41 +0530

    setState functional approach

--- ReactFreeCodeCamp/Advance React/src/tutorial/1-useState/setup/5-useState-counter.js
+++ ReactFreeCodeCamp/Advance React/src/tutorial/1-useState/setup/5-useState-counter.js
@@ -5,6 +5,15 @@ const UseStateCounter = () => {
   const reset = () => {
     setValue(0);
   };
+
+  const complexDecrease = () => {
+    setTimeout(() => {
+      // setValue(value - 1);       //SetValue is Asynchrounous Function
+      setValue((prevValue) => {
+        return prevValue - 1;
+      });
+    }, 2000);
+  };
   return (
     <>
       <h2>Regular Counter</h2>
@@ -27,6 +36,12 @@ const UseStateCounter = () => {
         }}
       >
         Increase
+      </button>
+
+      <h2>Complex Counter</h2>
+      <h1>{value}</h1>
+      <button className="btn" onClick={complexDecrease}>
+        Decrease
       </button>
     </>
   );
