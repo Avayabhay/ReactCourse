ff45c048005df38622419fade97563c17629aee2
Abhay Sharma <avay.abhay@gmail.com>
2021-08-05 20:48:55 +0530

    useState Example on Objects

--- ReactFreeCodeCamp/Advance React/src/tutorial/1-useState/setup/4-useState-object.js
+++ ReactFreeCodeCamp/Advance React/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Andrew",
+    age: 22,
+    message: "This is my Message",
+  });
+
+  const changeMessage = () => {
+    setPerson({ ...person, message: "new Message" });
+  };
+  return (
+    <>
+      <h2>{person.name}</h2>
+      <h2>{person.age}</h2>
+      <h2>{person.message}</h2>
+      <button type="button" className="btn" onClick={changeMessage}>
+        Change the Message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
