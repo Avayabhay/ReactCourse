e4a5642053d272ae37cd0f0b4bec9e23c5a2cf07
Abhay Sharma <avay.abhay@gmail.com>
2021-08-12 11:28:49 +0530

    Clean Up Function in UseEffect

--- ReactFreeCodeCamp/Advance React/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ ReactFreeCodeCamp/Advance React/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+  console.log(size);
+  const changeSize = () => {
+    setSize(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", changeSize);
+
+    return () => {
+      console.log("CleanUp");
+      window.removeEventListener("resize", changeSize);
+    };
+  });
+  return (
+    <>
+      <h2>useEffect cleanup</h2>
+      <h1>{size} PX</h1>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
