25b7544b784743f9b87b419d9b24654deb7d47b9
Abhay Sharma <avay.abhay@gmail.com>
2021-08-14 09:47:07 +0530

    Short-Circuit Evaluation

--- ReactFreeCodeCamp/Advance React/src/App.js
+++ ReactFreeCodeCamp/Advance React/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import Setup from "./tutorial/1-useState/setup/5-useState-counter";
 // import UseEffect from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
-import ConditionalRendering from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
+import ConditionalRendering from "./tutorial/3-conditional-rendering/setup/2-short-circuit";
 
 function App() {
   return (
--- ReactFreeCodeCamp/Advance React/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ ReactFreeCodeCamp/Advance React/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("Abhay");
+  const firstValue = text || "hello world";
+  const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      <h1>{text} is the Text</h1>
+      <hr />
+      <h1>first Value: {firstValue}</h1>
+      <hr />
+      <h1>Second Value : {secondValue}</h1>
+      <hr />
+      <h2>{text || "OR CASE"}</h2>
+      <h2>{!text && "AND CASE"}</h2>
+    </>
+  );
 };
 
 export default ShortCircuit;
