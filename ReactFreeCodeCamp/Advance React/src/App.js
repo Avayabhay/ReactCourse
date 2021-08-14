4d46c9044edb17b8b1615a032593d2ca20cd6a2d
Abhay Sharma <avay.abhay@gmail.com>
2021-08-14 08:14:15 +0530

    Multiple Returns Fetching Data

--- ReactFreeCodeCamp/Advance React/src/App.js
+++ ReactFreeCodeCamp/Advance React/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 // import Setup from "./tutorial/1-useState/setup/5-useState-counter";
-import UseEffect from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
+// import UseEffect from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
+import ConditionalRendering from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
 
 function App() {
   return (
     <div className="container">
-      <UseEffect />
+      <ConditionalRendering />
     </div>
   );
 }
