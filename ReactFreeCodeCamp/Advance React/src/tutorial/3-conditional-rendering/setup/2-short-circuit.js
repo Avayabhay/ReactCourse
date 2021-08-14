f868fe692d5449eb159aa9cd2e4acc1d94c30b8e
Abhay Sharma <avay.abhay@gmail.com>
2021-08-14 14:45:04 +0530

    Ternary operator

--- ReactFreeCodeCamp/Advance React/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ ReactFreeCodeCamp/Advance React/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,20 +3,38 @@ import React, { useState } from "react";
 // ternary operator
 
 const ShortCircuit = () => {
-  const [text, setText] = useState("Abhay");
-  const firstValue = text || "hello world";
-  const secondValue = text && "hello world";
+  // const [text, setText] = useState("Abhay");
+  // const firstValue = text || "hello world";
+  // const secondValue = text && "hello world";
+  const [isError, setIsError] = useState(false);
 
   return (
     <>
-      <h1>{text} is the Text</h1>
+      {/* <h1>{text} is the Text</h1>
       <hr />
       <h1>first Value: {firstValue}</h1>
       <hr />
-      <h1>Second Value : {secondValue}</h1>
+      <h1>Second Value : {secondValue}</h1> */}
       <hr />
-      <h2>{text || "OR CASE"}</h2>
-      <h2>{!text && "AND CASE"}</h2>
+      {/* <h2>{text || "OR CASE"}</h2>
+      <h2>{!text && "AND CASE"}</h2> */}
+
+      <button
+        type="button"
+        className="btn"
+        onClick={() => setIsError(!isError)}
+      >
+        Toggle Error
+      </button>
+      {isError && <h1>Error..</h1>}
+
+      {isError ? (
+        <div>
+          <h4>there is an error</h4>
+        </div>
+      ) : (
+        <div>IT is Fine</div>
+      )}
     </>
   );
 };
