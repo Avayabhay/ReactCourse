67d0c4a243bc23367a285018b61da168a4823ca5
Abhay Sharma <avay.abhay@gmail.com>
2021-08-19 13:58:58 +0530

    Multiple Inputs

--- ReactFreeCodeCamp/Advance React/src/App.js
+++ ReactFreeCodeCamp/Advance React/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 // import Setup from "./tutorial/1-useState/setup/5-useState-counter";
 // import UseEffect from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
 // import ConditionalRendering from "./tutorial/3-conditional-rendering/setup/3-show-hide";
-import Forms from "./tutorial/4-forms/setup/1-controlled-inputs";
+import Forms from "./tutorial/4-forms/setup/2-multiple-inputs";
 
 function App() {
   return (
