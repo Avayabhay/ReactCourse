ed81ca1c6b51b79141bd659cfdab76266abe89f8
Abhay Sharma <avay.abhay@gmail.com>
2021-08-24 10:57:33 +0530

    useRef

--- ReactFreeCodeCamp/Advance React/src/App.js
+++ ReactFreeCodeCamp/Advance React/src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 // import Setup from "./tutorial/1-useState/setup/5-useState-counter";
 // import UseEffect from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
 // import ConditionalRendering from "./tutorial/3-conditional-rendering/setup/3-show-hide";
-import Forms from "./tutorial/4-forms/setup/2-multiple-inputs";
+// import Forms from "./tutorial/4-forms/setup/2-multiple-inputs";
+import UseRef from "./tutorial/5-useRef/setup/1-useRef-basics";
 
 function App() {
   return (
     <div className="container">
-      <Forms />
+      <UseRef />
     </div>
   );
 }
