6d32d87b7d766c079b6d7a88ecc360029cf60e07
Abhay Sharma <avay.abhay@gmail.com>
2021-08-24 20:13:49 +0530

    useReducer- useState setup

--- ReactFreeCodeCamp/Advance React/src/App.js
+++ ReactFreeCodeCamp/Advance React/src/App.js
@@ -3,12 +3,13 @@ import React from "react";
 // import UseEffect from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
 // import ConditionalRendering from "./tutorial/3-conditional-rendering/setup/3-show-hide";
 // import Forms from "./tutorial/4-forms/setup/2-multiple-inputs";
-import UseRef from "./tutorial/5-useRef/setup/1-useRef-basics";
+// import UseRef from "./tutorial/5-useRef/setup/1-useRef-basics";
+import UseReducer from "./tutorial/6-useReducer/setup";
 
 function App() {
   return (
     <div className="container">
-      <UseRef />
+      <UseReducer />
     </div>
   );
 }
--- ReactFreeCodeCamp/Advance React/src/tutorial/6-useReducer/setup/index.js
+++ ReactFreeCodeCamp/Advance React/src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,44 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name, setName] = useState("");
+  const [people, setPeople] = useState(data);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("ADDED");
+    if (name) {
+      setShowModal(true);
+      setPeople([...people, { id: new Date().getTime().toString(), name }]);
+      setName("");
+    } else {
+      setShowModal(true);
+    }
+  };
+  return (
+    <>
+      {showModal && <Modal />}
+      <form onSubmit={handleSubmit} className="form">
+        <input type="text" onChange={(e) => setName(e.target.value)} />
+        <button type="submit" className="btn">
+          Add
+        </button>
+      </form>
+      <div>
+        {people.map((person) => {
+          return (
+            <div key={person.id}>
+              <h4>{person.name}</h4>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Index;
